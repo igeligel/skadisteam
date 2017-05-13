a1e413192e6f3da8d6470cd6e088b5b290725f6c
Kevin Peters <git.igeligel@googlemail.com>
2017-05-14 01:11:13 +0200

    chore(project)
    add submodules

---
+++ src/FullSolution/skadisteam.console/skadisteam.console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace skadisteam.console
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
