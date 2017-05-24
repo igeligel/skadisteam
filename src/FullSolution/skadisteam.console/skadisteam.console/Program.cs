f99a0442a81b7d1eeebd7b08e645d96b1e0655a7
Kevin Peters <git.igeligel@googlemail.com>
2017-05-25 00:59:37 +0200

    chore(project)
    first project structure

--- src/FullSolution/skadisteam.console/skadisteam.console/Program.cs
+++ src/FullSolution/skadisteam.console/skadisteam.console/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using skadisteam.console.Settings;
+using skadisteam.login;
+using skadisteam.login.Models;
+using skadisteam.trade;
+using skadisteam.inventory.Models;
+using skadisteam.inventory;
 
 namespace skadisteam.console
 {
     internal class Program
     {
+        // ReSharper disable once UnusedMember.Local
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Loader.LoadSettings();
+            Console.ReadKey();
+            var skadiLoadItemInventoryConfiguration =
+                new SkadiLoadItemInventoryConfiguration
+                {
+                    AppId = 730,
+                    ContextId = 2,
+                    PartnerCommunityId = 76561198028630048
+                };
+            var inventory =
+                SkadiInventoryClient.LoadInventoryItems(
+                    skadiLoadItemInventoryConfiguration);
+            Console.ReadKey();
+            var skadiLoginData = new SkadiLoginData
+            {
+                Username = ConsoleSettings.Instance["username"],
+                Password = ConsoleSettings.Instance["username"],
+                SharedSecret = ConsoleSettings.Instance["username"]
+            };
+            var skadiLogin = new SkadiLogin();
+            var skadiLoginResponse = skadiLogin.Execute(skadiLoginData);
+            var skadiTradeClient = new SkadiTradeClient(skadiLoginResponse);
+            var basicOffers = skadiTradeClient.GetBasicTradeOffers();
             Console.ReadKey();
         }
     }
---
+++ src/FullSolution/skadisteam.console/skadisteam.console/Settings/ConsoleSettings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace skadisteam.console.Settings
+{
+    internal class ConsoleSettings
+    {
+        internal static Dictionary<string, string> Instance = new Dictionary<string, string>();
+    }
+}
